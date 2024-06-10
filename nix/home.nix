d984d0ac02924ed9346db7f3945c879161a6f362
Aspian <p.aspian1738@gmail.com>
2024-06-10 17:05:36 +0800

    update home.nix

--- nix/home.nix
+++ nix/home.nix
@@ -2,11 +2,16 @@
 
 let
   USER = builtins.getEnv "USER";
-
+  starship_session_key = builtins.getEnv "STARSHIP_SESSION_KEY";
   yt_dest = "/home/${USER}/Share/youtube/raw/";
 in
 
 {
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
   nixpkgs.config.allowUnfree = true;
 
   home = {
@@ -25,12 +30,18 @@ in
     #   # '';
     # };
 
+    sessionVariables = {
+      # STARSHIP_CONFIG = ".config/starship/config.toml";
+      # STARSHIP_CACHE = ".cache/starship/session_${starship_session_key}.log";
+      EDITOR = "nvim";
+    };
+
     packages = [
       # # It is sometimes useful to fine-tune packages, for example, by applying
       # # overrides. You can do that directly here, just don't forget the
       # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
       # # fonts?
-      # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" "0xProto" ]; })
+      (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" "0xProto" ]; })
 
       # Archive
       pkgs.bzip2
@@ -53,6 +64,7 @@ in
 
       # Monitor
       pkgs.bottom
+      # pkgs.gotop
       pkgs.htop
       # pkgs.iftop
       # pkgs.iotop
@@ -85,7 +97,7 @@ in
       # pkgs.steghide
 
       pkgs.neofetch
-      pkgs.nerdfonts #x!
+      # pkgs.nerdfonts #x!
       # pkgs.ollama
       # pkgs.ventoy-full
       # pkgs.media-downloader
@@ -103,7 +115,7 @@ in
       #   org.gradle.daemon.idletimeout=3600000
       # '';
 
-      ".config/nixpkgs/config.nix".text = "{ allowUnfree = true; }";
+      # ".config/nixpkgs/config.nix".text = "{ allowUnfree = true; }";
     };
   };
 
@@ -127,12 +139,11 @@ in
     };
 
     neovim = {
+      # https://www.youtube.com/live/lZshGG4Mcws?si=RYcPcNlWpn_RVC0E 1:33:00
       enable = true;
       viAlias = true;
       vimAlias = true;
-      withPython3 = true;
-
-      # coc.enable = false;
+      withPython3 = false;
 
       plugins = with pkgs.vimPlugins; [
         coc-nvim
@@ -140,16 +151,21 @@ in
         nvim-cmp
         nvim-lspconfig
         nvim-treesitter
-        # nvim-treesitter-parsers.python
         nvim-surround
 
         vim-airline
         vim-airline-clock
-        # vim-commentary
-        # vim-fugitive
-        # vim-gitgutter
-        # vim-indent-guides
+        vim-commentary
+        vim-fugitive
+        vim-gitgutter
+        vim-indent-guides
 
+        # {
+        #   plugin = cmp-nvim-lsp;
+        #   type = "lua";
+        #   config = ''
+        #   '';
+        # }
         {
           plugin = dracula-nvim;
           config = ''
@@ -188,24 +204,69 @@ in
     };
 
     starship = {
-      enable = true;
+      enable = false;
       enableZshIntegration = true;
+      # settings = {};
+      # https://starship.rs/config/
     };
 
     tmux = {
-      enable = false;
+      enable = true;
       mouse = true;
+      clock24 = true;
+      baseIndex = 1;
       shortcut = "a";
-      extraConfig = ''
-        set -g base-index 1
-        setw -g pane-base-index 1
-      '';
+      shell = "${pkgs.zsh}/bin/zsh";
+      # terminal = "screen-256color";
+
       plugins = with pkgs.tmuxPlugins; [
         better-mouse-mode
+        logging
+        # net-speed
+        # nord
         pain-control
         prefix-highlight
         sensible
+        sidebar
+        t-smart-tmux-session-manager
         yank
+
+        {
+          # https://draculatheme.com/tmux
+          plugin = dracula;
+          extraConfig = ''
+            # set -g @dracula-plugins "git ssh-session network-bandwidth ram-usage time"
+            set -g @dracula-plugins "git ssh-session network-bandwidth time"
+
+            set -g @dracula-border-contrast true
+            set -g @dracula-left-icon-padding 0
+            set -g @dracula-refresh-rate 5
+            set -g @dracula-show-battery false
+            set -g @dracula-show-powerline true
+            set -g @dracula-show-left-icon N
+
+            # Device
+            set -g @dracula-ram-usage-label ""
+
+            # Network
+            set -g @dracula-network-bandwidth-interval 0
+            set -g @dracula-show-ssh-session-port true
+
+            # Time
+            set -g @dracula-show-timezone false
+            set -g @dracula-day-month false
+            set -g @dracula-military-time true
+            set -g @dracula-time-format "%H:%M"
+
+            # Git
+            set -g @dracula-git-disable-status true
+            set -g @dracula-git-show-current-symbol $
+            set -g @dracula-git-show-diff-symbol !
+            set -g @dracula-git-no-repo-message "-"
+            set -g @dracula-git-no-untracked-files true
+            set -g @dracula-git-show-remote-status false
+          '';
+        }
       ];
     };
 
@@ -256,6 +317,14 @@ in
         rm = "trash-put";
       };
 
+      # loginShellInit = ''
+      #   echo "Login"
+      # '';
+
+      # shellInit = ''
+      #   echo "Init!"
+      # '';
+
       history = {
         ignoreDups = true;
         extended = true;
@@ -275,5 +344,4 @@ in
       };
     };
   };
-  # https://www.youtube.com/live/lZshGG4Mcws?si=RYcPcNlWpn_RVC0E 1:33:00
 }
