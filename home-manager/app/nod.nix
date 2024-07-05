f2e9ed73ca82b57df476a67b6b7b3be48e273c86
Aspian <p.aspian1738@gmail.com>
2024-07-05 09:18:50 +0800

    home-manager, sshd

--- home-manager/app/nod.nix
+++ home-manager/app/nod.nix
@@ -4,13 +4,23 @@
   imports = [
     ./shell/ts.nix
     ./shell/bash.nix
+    ./shell/zsh.nix
     ./editor/neovim.nix
+    ./core.nix
   ];
 
   home.packages = with pkgs; [
     apt
-    busybox
+    # busybox
     dpkg
     nano
   ];
+
+  home.file.".ssh/sshd_config".text = ''
+    PrintMotd yes
+    PasswordAuthentication no
+    HostKey /data/data/com.termux.nix/files/home/.ssh/ssh_host_rsa_key
+    Port 3022
+
+  '';
 }
