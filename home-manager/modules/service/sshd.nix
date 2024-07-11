1864e56bdcb3ed57dec608d5631d9852c1b7d48b
Aspian <p.aspian1738@gmail.com>
2024-07-12 00:24:08 +0800

    sshd

---
+++ home-manager/modules/service/sshd.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, lib, ... }:
+
+with lib; let cfg = config.service.sshd; in
+
+{
+  options.service.sshd = {
+    enable = mkEnableOption "sshd";
+
+    port = mkOption {
+      type = types.port;
+      default = 2222;
+    };
+
+    banner = mkOption {
+      type = types.nullOr types.port;
+      example = "/etc/banner";
+    };
+
+    addressFamily = mkOption {
+      type = types.enum [ "any" "inet" "inet6" ];
+      default = "any";
+    };
+  };
+}
