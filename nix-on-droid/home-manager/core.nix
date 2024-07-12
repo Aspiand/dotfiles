dd4a4c8cb055591a231a1aff3d49bad9f58d038d
Aspian <p.aspian1738@gmail.com>
2024-07-12 10:38:39 +0800

    nod: gpg, pass

--- nix-on-droid/home-manager/core.nix
+++ nix-on-droid/home-manager/core.nix
@@ -11,5 +11,17 @@
     which
   ];
 
-  programs.gpg.enable = true;
+  programs.gpg = {
+    enable = true;
+    homedir = "${config.home.homeDirectory}/.local/data/gnupg";
+  };
+
+  programs.password-store = {
+    enable = true;
+    settings = {
+      PASSWORD_STORE_CLIP_TIME = 120;
+      PASSWORD_STORE_GENERATED_LENGTH = 30;
+      PASSWORD_STORE_DIR = "$HOME/.local/data/password_store/";
+    };
+  };
 }
