579f85866af12e67c436ca1c3a3a33aa609e0461
Aspian <p.aspian1738@gmail.com>
2024-07-23 20:58:48 +0800

    dahlah

---
+++ home-manager/modules/utils/gpg.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, lib, ... }:
+
+with lib;
+
+let
+  cfg = config.utils.gpg;
+  homeDir = "${config.home.homeDirectory}/.local/data/gnupg";
+in
+
+{
+  # options.utils.gpg = {
+  #   enable = mkEnableOption "GnuPG";
+  # };
+
+  options.utils.gpg.enable = mkEnableOption "GnuPG";
+
+  config = mkIf cfg.enable {
+    home.packages = [ pkgs.pinentry-tty ];
+
+    programs.gpg = {
+      enable = true;
+      homedir = homeDir;
+    };
+
+    home.file."${homeDir}/gpg-agent.conf".text = ''
+      pinentry-program ${pkgs.pinentry-tty}
+      enable-ssh-support
+    '';
+  };
+}
