d9c7fae3509fe31d5e4b772a9f932d30827be2f8
Aspian <p.aspian1738@gmail.com>
2024-07-27 12:48:45 +0800

    ffm v3

--- sh/ffm.sh
+++ sh/ffm.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-readonly VERSION=2.1
+readonly VERSION=3.0
 readonly ROOT=$(dirname "${BASH_SOURCE[0]}")
 
 get_permission() {
@@ -23,47 +23,38 @@ for config_path in "${CONFIG_PATH[@]}"; do
     [ -f "$config_path" ] && source "$config_path"
 done
 
-# Parsing argument
-# https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
-
-echo "Folders:"
-
 for folder in "${FFM_FOLDERS[@]}"; do
-    read -r path permission <<< "$folder"
-    echo -n "  $path/ ($permission): "
+    read -r path apermission auser agroup <<< "$folder" # After
+    read -r buser bgroup bpermission <<< "$(get_permission "$path")" # Before
 
     if [ ! -d "$path" ]; then
-        echo "[mkdir]"
-        mkdir -pm "$permission" "$path"
-    elif [ "$(stat -c "%a" "$path")" != "$permission" ]; then
-        echo "[chmod($(stat -c "%a" "$path"))]"
-        chmod "$permission" "$path"
-    else
-        echo "[ok]"
+        mkdir -vpm "$apermission" "$path"
+    elif [ "$bpermission" != "$apermission" ]; then
+        chmod --verbose "$apermission" "$path"
+    fi
+
+    if [ "$buser" != "$auser" ] || [ "$bgroup" != "$agroup" ]; then
+        chown --verbose "${auser}:${agroup}" "$path"
     fi
 done
-
-echo "Files:"
 
 for file in "${FFM_FILES[@]}"; do
-    read -r path permission <<< "$file"
-    echo -n "  $path ($permission): "
+    read -r path apermission auser agroup <<< "$file" # After
+    read -r buser bgroup bpermission <<< "$(get_permission "$path")" # Before
 
     if [ ! -f "$path" ]; then
-        echo "[not found]"
-    elif [ "$(stat -c "%a" "$path")" != "$permission" ]; then
-        echo "[chmod($(stat -c "%a" "$path"))]"
-        chmod "$permission" "$path"
-    else
-        echo "[ok]"
+        echo "$path not found!"
+    elif [ "$bpermission" != "$apermission" ]; then
+        chmod --verbose "$apermission" "$path"
+    fi
+
+    if [ "$buser" != "$auser" ] || [ "$bgroup" != "$agroup" ]; then
+        chown --verbose "${auser}:${agroup}" "$path"
     fi
 done
-
-echo "Symlinks:"
 
 for symlinks in "${FFM_SYMLINKS[@]}"; do
     read -r source destination <<< "$symlinks"
-    echo -n "  $source -> $destination: "
 
     if [ -e "$destination" ] && [ "$(readlink -f "$destination")" == "$source" ]; then
         echo "[ok]"
@@ -72,20 +63,12 @@ for symlinks in "${FFM_SYMLINKS[@]}"; do
     elif [ -e "$destination" ]; then
         echo "[destination is available]"
     elif [ ! -e "$destination" ] && [ "$(readlink -f "$destination")" != "$source" ]; then
-        ln -s "$source" "$destination"
-        echo "[create link]"
-    else
-        echo "[ok]"
+        ln -vs "$source" "$destination"
     fi
 done
 
-echo "Recursive:"
 for recursive in "${FFM_RECURSIVE[@]}"; do
     read -r path folder file <<< "$recursive"
-    echo "  $path > $folder | $file"
-
-    # find $path -type d -not -perm $folder -exec \
-    #     echo "$path ($(stat -c "%a" "$path")): " \;
 
     find "$path" -type d -not -perm "$folder" -exec chmod --verbose "$folder" {} \;
     find "$path" -type f -not -perm "$file" -exec chmod --verbose "$file" {} \;
