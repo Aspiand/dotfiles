403e1ae111502fd4c98c6d53bc48ed249090c9fc
Aspian <p.aspian1738@gmail.com>
2024-08-04 18:21:45 +0800

    hm: yt-dlp

--- home-manager/modules/utils/yt-dlp.nix
+++ home-manager/modules/utils/yt-dlp.nix
@@ -9,6 +9,10 @@ in
 {
   options.utils.yt-dlp = {
     enable = mkEnableOption "yt-dlp";
+    downloader = mkOption {
+      type = types.str;
+      default = "aria2c";
+    };
     path = mkOption {
       type = types.str;
       default = "$HOME/Downloads";
@@ -21,21 +25,30 @@ in
 
     programs.yt-dlp = {
       enable = true;
-      settings = {
-        paths = cfg.path;
-        output = "%(title)s.%(ext)s";
+      settings = mkMerge [
+        {
+          paths = cfg.path;
+          output = "%(title)s.%(ext)s";
 
-        embed-chapters = true;
-        embed-metadata = true;
-        embed-subs = true;
-        embed-thumbnail = true;
+          embed-chapters = true;
+          embed-metadata = true;
+          embed-subs = true;
+          embed-thumbnail = true;
 
-        format = "bestvideo*+bestaudio/best";
-        merge-output-format = "mkv";
+          format = "bestvideo*+bestaudio/best";
+          merge-output-format = "mkv";
+        }
 
-        downloader = "aria2c";
-        downloader-args = "aria2c:'-x16 -s16 -c'";
-      };
+        (mkIf (cfg.downloader == "aria2c") {
+          downloader = "aria2c";
+          downloader-args = "aria2c:'-x16 -s16 -c'";
+        })
+
+        (mkIf (cfg.downloader == "wget") {
+          downloader = "wget";
+          downloader-args = "wget:'--retry-connrefused --continue'";
+        })
+      ];
 
       extraConfig = "--sub-langs all,-live_chat";
     };
