f6b5626fb1a614a6ac72527723d61b339ec1dd09
Aspian <p.aspian1738@gmail.com>
2024-10-12 13:59:04 +0800

    {librewolf, neovim, codium}.desktop

--- home-manager/modules/editor/vscode.nix
+++ home-manager/modules/editor/vscode.nix
@@ -6,6 +6,43 @@ with lib; let cfg = config.programs.utils.vscode; in
   options.programs.utils.vscode.enable = mkEnableOption "VSCodium";
 
   config = mkIf cfg.enable {
+    home.file.".local/share/applications/codium.desktop".text = ''
+      [Desktop Entry]
+      Actions=new-empty-window
+      Categories=Utility;TextEditor;Development;IDE
+      Comment=Code Editing. Redefined.
+      Exec=codium %F
+      GenericName=Text Editor
+      Icon=vscodium
+      Keywords=vscode
+      Name=VSCodium
+      StartupNotify=true
+      StartupWMClass=vscodium
+      Type=Application
+      Version=1.4
+
+      [Desktop Action new-empty-window]
+      Exec=codium --new-window %F
+      Icon=vscodium
+      Name=New Empty Window
+    '';
+
+    home.file.".local/share/applications/codium-url-handler.desktop".text = ''
+      [Desktop Entry]
+      Categories=Utility;TextEditor;Development;IDE
+      Comment=Code Editing. Redefined.
+      Exec=codium --open-url %U
+      GenericName=Text Editor
+      Icon=vscodium
+      Keywords=vscode
+      MimeType=x-scheme-handler/vscodium
+      Name=VSCodium - URL Handler
+      NoDisplay=true
+      StartupNotify=true
+      Type=Application
+      Version=1.4
+    '';
+
     programs.vscode = {
       enable = true;
       package = pkgs.vscodium;
