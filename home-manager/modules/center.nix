aaa3d170b6504db3e63be861fde9c9ab7097d906
Aspian <p.aspian1738@gmail.com>
2024-12-25 11:10:02 +0800

    refactor home-manager structure - part 3

--- home-manager/modules/center.nix
+++ home-manager/modules/center.nix
@@ -6,6 +6,19 @@ with lib;
   options = {
     programs = {
       ssh.control = mkEnableOption "SSH Control";
+
+      yt-dlp = {
+        path = mkOption {
+          type = types.str;
+          default = "${config.home.homeDirectory}/Downloads/";
+          description = "The paths where the files should be downloaded";
+        };
+
+        downloader = mkOption {
+          type = with types; nullOr str;
+          default = null;
+        };
+      };
     };
 
     shell.nix-path = mkOption {
@@ -27,6 +40,7 @@ with lib;
       };
     })
 
+    # Shell
     (let path = config.shell.nix-path; in {
       programs.bash.bashrcExtra = ''
         source ${path}
@@ -56,12 +70,126 @@ with lib;
     })
 
     (mkIf cfg.ssh.enable {
-      home.activation.ssh_setup = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      home.activation.sshSetup = lib.hm.dag.entryAfter ["writeBoundary"] ''
         if [ -d "$HOME/.ssh" ]; then
           find "$HOME/.ssh" -type d -not -perm "700" -exec chmod -v 700 {} \;
           find "$HOME/.ssh" -type f -not -perm "600" -exec chmod -v 600 {} \;
         fi
       '';
     })
+
+    (mkIf true {
+      home.packages = with pkgs; [
+        # Archive
+        bzip2
+        bzip3
+        gzip
+        unrar
+        unzip
+        gnutar
+        xz
+        zip
+        zstd
+
+        # Monitor
+        bottom
+        gotop
+        # nyx
+
+        # Multimedia
+        # exiftool
+        ffmpeg
+
+        # Network
+        aria2
+        sshfs
+        wget
+
+        # Utils
+        bat
+        gitui
+      ];
+    })
+
+    # General
+    {
+      programs = {
+        eza = {
+          git = true;
+          icons = "always";
+          enableZshIntegration = true;
+          enableBashIntegration = true;
+          extraOptions = [
+            "--group"
+            "--group-directories-first"
+            "--mounts"
+            "--no-quotes"
+          ];
+        };
+
+        fzf = {
+          enableZshIntegration = true;
+          enableBashIntegration = true;
+          tmux.enableShellIntegration = true;
+          defaultOptions = [
+            "--border"
+            "--height 60%"
+          ];
+
+          tmux.shellIntegrationOptions = [];
+        };
+
+        yazi = {
+          enableBashIntegration = true;
+          enableZshIntegration = true;
+          settings = {
+            log.enable = true;
+            manager = {
+              show_hidden = true;
+              sort_by = "alphabetical";
+              sort_dir_first = true;
+              sort_reverse = false;
+              show_symlink = true;
+            };
+          };
+        };
+
+        yt-dlp = {
+          settings = mkMerge [
+            {
+              paths = cfg.yt-dlp.path;
+              output = "%(title)s.%(ext)s";
+
+              embed-chapters = true;
+              embed-metadata = true;
+              embed-subs = true;
+              embed-thumbnail = true;
+
+              format = "bestvideo*+bestaudio/best";
+              merge-output-format = "mkv/mp4";
+            }
+
+            (mkIf (cfg.yt-dlp.downloader == "aria2c") {
+              downloader = "aria2c";
+              downloader-args = "aria2c:'-x8 -s8 -c'";
+            })
+
+            (mkIf (cfg.yt-dlp.downloader == "wget") {
+              downloader = "wget";
+              downloader-args = "wget:'--retry-connrefused --continue'";
+            })
+          ];
+
+          extraConfig = ''
+            --sub-langs all,-live_chat
+          '';
+        };
+
+        zoxide = {
+          enableBashIntegration = true;
+          enableZshIntegration = true;
+        };
+      };
+    }
   ];
 }
--- home-manager/modules/tmux.nix
+++ home-manager/modules/tmux.nix
@@ -1,81 +1,67 @@
-{ config, pkgs, lib, ... }:
-
-with lib; let cfg = config.programs.utils.tmux; in
+{ pkgs, ... }:
 
 {
-  options.programs.utils.tmux = {
-    enable = mkEnableOption "Tmux";
-    shell = mkOption {
-      type = with types; nullOr str;
-      default = null;
-    };
-  };
+  programs.tmux = {
+    mouse = true;
+    clock24 = true;
+    newSession = true;
+    aggressiveResize = true;
+    baseIndex = 1;
+    shortcut = "a";
+    terminal = "screen-256color";
 
-  config = mkIf cfg.enable {
-    programs.tmux = {
-      enable = true;
-      mouse = true;
-      clock24 = true;
-      newSession = true;
-      aggressiveResize = true;
-      baseIndex = 1;
-      shortcut = "a";
-      terminal = "screen-256color";
-      shell = cfg.shell;
+    extraConfig = ''
+      set-option -g status-position top
+    '';
 
-      extraConfig = ''
-        set-option -g status-position top
-      '';
+    plugins = with pkgs.tmuxPlugins; [
+      better-mouse-mode
+      continuum
+      copycat
+      logging
+      pain-control
+      prefix-highlight
+      resurrect
+      sensible
+      sidebar
+      yank
 
-      plugins = with pkgs.tmuxPlugins; [
-        better-mouse-mode
-        continuum
-        copycat
-        logging
-        pain-control
-        prefix-highlight
-        resurrect
-        sensible
-        sidebar
-        yank
+      {
+        # https://draculatheme.com/tmux
+        plugin = dracula;
+        extraConfig = ''
+          set -g @dracula-plugins "ssh-session network-bandwidth"
 
-        {
-          # https://draculatheme.com/tmux
-          plugin = dracula;
-          extraConfig = ''
-            set -g @dracula-plugins "ssh-session network-bandwidth"
+          set -g @dracula-border-contrast true
+          set -g @dracula-left-icon-padding 0
+          set -g @dracula-refresh-rate 5
+          set -g @dracula-show-battery false
+          set -g @dracula-show-powerline true
+          set -g @dracula-show-left-icon ☪
 
-            set -g @dracula-border-contrast true
-            set -g @dracula-left-icon-padding 0
-            set -g @dracula-refresh-rate 5
-            set -g @dracula-show-battery false
-            set -g @dracula-show-powerline true
-            set -g @dracula-show-left-icon ☪
+          # Device
+          set -g @dracula-ram-usage-label ""
 
-            # Device
-            set -g @dracula-ram-usage-label ""
+          # Network
+          # set -g @dracula-network-bandwidth eth0
+          set -g @dracula-network-bandwidth-interval 0.5
+          set -g @dracula-show-ssh-session-port true
 
-            # Network
-            # set -g @dracula-network-bandwidth eth0
-            set -g @dracula-network-bandwidth-interval 0.5
-            set -g @dracula-show-ssh-session-port true
+          # Time
+          set -g @dracula-show-timezone false
+          set -g @dracula-day-month false
+          set -g @dracula-military-time true
+          set -g @dracula-time-format "%H:%M"
 
-            # Time
-            set -g @dracula-show-timezone false
-            set -g @dracula-day-month false
-            set -g @dracula-military-time true
-            set -g @dracula-time-format "%H:%M"
-
-            # Git
-            set -g @dracula-git-disable-status true
-            set -g @dracula-git-show-current-symbol ✓
-            set -g @dracula-git-show-diff-symbol !
-            set -g @dracula-git-no-repo-message "-"
-            set -g @dracula-git-no-untracked-files true
-            set -g @dracula-git-show-remote-status false
-          '';
-        }
-      ];
-    };
+          # Git
+          set -g @dracula-git-disable-status true
+          set -g @dracula-git-show-current-symbol ✓
+          set -g @dracula-git-show-diff-symbol !
+          set -g @dracula-git-no-repo-message "-"
+          set -g @dracula-git-no-untracked-files true
+          set -g @dracula-git-show-remote-status false
+        '';
+      }
+    ];
   };
 }
