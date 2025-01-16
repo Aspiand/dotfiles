9c85f5b0c8841e3b28ed89737519f66ee6d4d1df
Aspian <p.aspian1738@gmail.com>
2025-01-16 08:59:43 +0800

    ...

--- home-manager/modules/password-store.nix
+++ home-manager/modules/password-store.nix
@@ -2,13 +2,17 @@
 
 with lib;
 
+let
+  cfg = config.programs.password-store;
+in
+
 {
   options.programs.password-store.dir = mkOption {
     type = types.path;
     default = "${config.xdg.dataHome}/password_store";
   };
 
-  config = mkIf config.programs.password-store.enable {
+  config = mkIf cfg.enable {
     programs.password-store.settings = {
       PASSWORD_STORE_CLIP_TIME = "120";
       PASSWORD_STORE_GENERATED_LENGTH = "12";
@@ -16,9 +20,9 @@ with lib;
     };
 
     home.activation.passSetup = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      if [ -d '${dir}' ]; then
-        find ${dir} -type d -not -perm "700" -exec chmod -v 700 {} \;
-        find ${dir} -type f -not -perm "600" -exec chmod -v 600 {} \;
+      if [ -d '${cfg.dir}' ]; then
+        find ${cfg.dir} -type d -not -perm "700" -exec chmod -v 700 {} \;
+        find ${cfg.dir} -type f -not -perm "600" -exec chmod -v 600 {} \;
       fi
     '';
   };
