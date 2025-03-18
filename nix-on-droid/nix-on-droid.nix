3da217c191261ef9c2c66d159f7dfa82612b8970
Aspian <p.aspian1738@gmail.com>
2025-03-19 06:17:58 +0800

    nix-on-droid: clean

--- nix-on-droid/nix-on-droid.nix
+++ nix-on-droid/nix-on-droid.nix
@@ -51,20 +51,22 @@
           yt-dlp.path = "/data/data/com.termux.nix/files/home/storage/Share/YouTube/";
         };
 
-        services.sshd = {
-          enable = true;
-          port = 3022;
-          addressFamily = "inet";
-          dir = "${config.home.homeDirectory}/.ssh";
-        };
+        services = {
+          sshd = {
+            enable = true;
+            port = 3022;
+            addressFamily = "inet";
+            dir = "${config.home.homeDirectory}/.ssh";
+          };
 
-        services.gpg-agent = {
-          enable = false;
-          enableSshSupport = true;
-          enableBashIntegration = true;
-          pinentryPackage = pkgs.pinentry-tty;
-          defaultCacheTtl = 600;
-          defaultCacheTtlSsh = 600;
+          gpg-agent = {
+            enable = false;
+            enableSshSupport = true;
+            enableBashIntegration = true;
+            pinentryPackage = pkgs.pinentry-tty;
+            defaultCacheTtl = 600;
+            defaultCacheTtlSsh = 600;
+          };
         };
       };
   };
