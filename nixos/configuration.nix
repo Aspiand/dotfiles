845d57a26c83e5f88d08ea57ddb2eb5e50a52aca
Aspian <muhammad.aspian.d@gmail.com>
2025-04-28 08:10:33 +0800

    nixos: gnome

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,7 +7,7 @@
   nixpkgs.config.allowUnfree = true;
   security.rtkit.enable = true;
   time.timeZone = "Asia/Makassar";
-  home-manager.backupFileExtension = ".bak";
+  home-manager.backupFileExtension = "bak";
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -16,9 +16,7 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.11"; # Did you read the comment?
 
-  imports = [
-    ./hardware-configuration.nix
-  ];
+  imports = [ ./hardware-configuration.nix ];
 
   hardware.graphics = {
     enable = true;
@@ -52,7 +50,6 @@
     };
   };
 
-
   users.users.ao = {
     isNormalUser = true;
     description = "Aspian";
@@ -71,8 +68,8 @@
       mangohud
       # protonup-qt
       lutris
-      bottles
-      heroic
+      # bottles
+      # heroic
 
       gparted
     ];
