8345bfd1e04c8eef987268ecbaf29027857f0239
Aspian <muhammad.aspian.d@gmail.com>
2025-05-29 14:34:17 +0800

    nixos: switch back to 25.05
    [8a6327988d566b87b67d2c352e8ca33eb12884a8](https://github.com/Aspiand/dotfiles/commit/8a6327988d566b87b67d2c352e8ca33eb12884a8)

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,11 +2,13 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     grub2-themes = {
       url = "github:vinceliuice/grub2-themes";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -43,12 +45,12 @@
         };
       };
 
-      homeConfigurations = {
-        backupFileExtension = "bak";
-        ao = home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs { inherit system; };
-          modules = [ ./home.nix ];
-        };
-      };
+      # homeConfigurations = {
+      #   backupFileExtension = "bak";
+      #   ao = home-manager.lib.homeManagerConfiguration {
+      #     pkgs = import nixpkgs { inherit system; };
+      #     modules = [ ./home.nix ];
+      #   };
+      # };
     };
 }
