01dd57bb0a090ae97f14d96034af26497cbd0997
Aspian <p.aspian1738@gmail.com>
2025-07-01 14:26:48 +0800

    home-manager: add work profile

---
+++ home-manager/profiles/work.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ../. ];
+
+  fonts.fontconfig.enable = true;
+  nixpkgs.config.allowUnfree = true;
+  nix = {
+    package = pkgs.nix;
+    settings.experimental-features = [ "nix-command" "flakes" ];
+  };
+
+  home = {
+    username = "pc";
+    homeDirectory = "/home/pc";
+    stateVersion = "25.05";
+
+    shellAliases = {
+      hmbs = "nix run home-manager -- build switch";
+      hmg = "nix run home-manager -- generations";
+    };
+
+    packages = with pkgs; [
+      s3fs
+      rustic
+      python312
+      python312Packages.pip
+      python312Packages.virtualenv
+
+      distrobox
+      duf
+      nix-bash-completions
+    ];
+  };
+
+  programs = {
+    git.extraConfig.core.editor = "${pkgs.vscode}/bin/code --wait";
+
+    bash.enable = true;
+    tmux.enable = true;
+    tmux.shell = "${pkgs.bash}/bin/bash";
+
+    ssh = {
+      matchBlocks.github.identityFile = [
+        "~/.ssh/aspian_ed25519"
+      ];
+    };
+  };
+}
