9a5a46e98704349146cfd287d4a7098fd647d073
Aspian <muhammad.aspian.d@gmail.com>
2025-07-24 10:32:44 +0800

    home-manager(modules): disable

--- home-manager/modules/starship.nix
+++ home-manager/modules/starship.nix
@@ -16,18 +16,88 @@ with lib;
         format = "[$duration ](bold yellow)";
       };
 
+      git_branch = {
+        format = "[$symbol$branch(:$remote_branch)]($style) ";
+      };
+
+      memory_usage = {
+        disabled = false;
+        threshold = 80;
+        symbol = "󰍛 ";
+      };
+
       nix_shell = {
         disabled = false;
-        impure_msg = "[impure shell](bold red)";
-        pure_msg = "[pure shell](bold green)";
-        unknown_msg = "[unknown shell](bold yellow)";
-        format = "❄️ [$state( \($name\))](bold blue)";
+        symbol = "󰼪 ";
+        impure_msg = "[impure](bold red)";
+        pure_msg = "[pure](bold green)";
+        unknown_msg = "[unknown](bold yellow)";
+        # format = "❄️ [$state( \($name\))](bold blue)";
       };
 
       sudo = {
         disabled = false;
         format = "[ɫ ](bold red)";
       };
+
+      bun.disabled = true;
+      buf.disabled = true;
+      c.disabled = true;
+      cpp.disabled = true;
+      cmake.disabled = true;
+      conda.disabled = true;
+      crystal.disabled = true;
+      daml.disabled = true;
+      dart.disabled = true;
+      deno.disabled = true;
+      dotnet.disabled = true;
+      elixir.disabled = true;
+      elm.disabled = true;
+      erlang.disabled = true;
+      fennel.disabled = true;
+      gcloud.disabled = true;
+      gleam.disabled = true;
+      golang.disabled = true;
+      guix_shell.disabled = true;
+      gradle.disabled = true;
+      haskell.disabled = true;
+      haxe.disabled = true;
+      helm.disabled = true;
+      java.disabled = true;
+      julia.disabled = true;
+      kotlin.disabled = true;
+      lua.disabled = true;
+      meson.disabled = true;
+      mise.disabled = true;
+      mojo.disabled = true;
+      nats.disabled = true;
+      nim.disabled = true;
+      nodejs.disabled = true;
+      ocaml.disabled = true;
+      odin.disabled = true;
+      opa.disabled = true;
+      openstack.disabled = true;
+      perl.disabled = true;
+      php.disabled = true;
+      pixi.disabled = true;
+      pulumi.disabled = true;
+      purescript.disabled = true;
+      python.disabled = true;
+      quarto.disabled = true;
+      rlang.disabled = true;
+      raku.disabled = true;
+      red.disabled = true;
+      ruby.disabled = true;
+      rust.disabled = true;
+      scala.disabled = true;
+      solidity.disabled = true;
+      spack.disabled = true;
+      swift.disabled = true;
+      terraform.disabled = true;
+      typst.disabled = true;
+      vagrant.disabled = true;
+      vlang.disabled = true;
+      zig.disabled = true;
     };
   };
 }
