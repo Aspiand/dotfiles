ec388d12d2b58e6859e74e9cd6011953f9bc4b2d
Aspian <muhammad.aspian.d@gmail.com>
2025-08-10 19:57:49 +0800

    home-manager: add yuki profile

---
+++ home-manager/profiles/yuki.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ../. ];
+
+  fonts.fontconfig.enable = true;
+  nixpkgs.config.allowUnfree = true;
+
+  home = {
+    username = "yuki";
+    homeDirectory = "/home/yuki";
+    stateVersion = "25.05";
+
+    shellAliases = {
+      hmbs = "home-manager build switch --verbose";
+    };
+
+    packages = with pkgs; [
+      restic
+      rustic
+      distrobox
+      duf
+      nix-bash-completions
+    ];
+  };
+
+  programs = {
+    git.extraConfig.core.editor = "${pkgs.micro}/bin/micro --wait";
+
+    bash.enable = true;
+    tmux.enable = true;
+  };
+}
