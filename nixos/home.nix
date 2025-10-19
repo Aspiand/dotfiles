5bcc6709f9f69f34ecc83fb095478d452c687a95
Aspian <muhammad.aspian.d@gmail.com>
2025-10-19 20:11:41 +0800

    temporary disable

--- nixos/home.nix
+++ nixos/home.nix
@@ -152,7 +152,7 @@
     clamav.enable = true;
     gpg.enable = true;
     mycli.enable = true;
-    ssh.control = true;
+    ssh.control = false;
     tmux.enable = true;
     yt-dlp.enable = true;
     yt-dlp.downloader = "wget";
