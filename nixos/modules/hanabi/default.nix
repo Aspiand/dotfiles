4bdf0b7caff92e97c4aaaace8f8d8a657d676a18
Aspian <muhammad.aspian.d@gmail.com>
2026-03-26 19:42:59 +0800

    update hanabi

--- nixos/modules/hanabi/default.nix
+++ nixos/modules/hanabi/default.nix
@@ -1,6 +1,10 @@
-# source: https://discourse.nixos.org/t/how-to-have-libgtk-4-media-gstreamer-on-nixos/37548/17
+# source: https://discourse.nixos.org/t/how-to-have-libgtk-4-media-gstreamer-on-nixos/
 
-{pkgs, stdenv, fetchFromGitHub }:
+{
+  pkgs,
+  stdenv,
+  fetchFromGitHub,
+}:
 {
   hanabi = stdenv.mkDerivation rec {
     pname = "gnome-ext-hanabi";
@@ -14,62 +18,44 @@
       wrapGAppsHook4
       appstream-glib
       gobject-introspection
-#      makeWrapper
-#      pkg-config
       shared-mime-info
     ];
 
     buildInputs = with pkgs; [
-    # Video/Audio data composition framework tools like "gst-inspect", "gst-launch" ...
-    gst_all_1.gstreamer
-    # Common plugins like "filesrc" to combine within e.g. gst-launch
-    gst_all_1.gst-plugins-base
-    # Specialized plugins separated by quality
-    gst_all_1.gst-plugins-good
-    gst_all_1.gst-plugins-bad
-    gst_all_1.gst-plugins-ugly
-    # Plugins to reuse ffmpeg to play almost every video format
-    gst_all_1.gst-libav
-    # Support the Video Audio (Hardware) Acceleration API
-    gst_all_1.gst-vaapi
-#    gst_all_1.gst-plugins-rs
-    clapper
-    gjs
-    gtk4
-    glib
-#    libadwaita
-#    libGL
-#    libsoup
-    wayland
-    wayland-protocols
+      gst_all_1.gstreamer
+      gst_all_1.gst-plugins-base
+      gst_all_1.gst-plugins-good
+      gst_all_1.gst-plugins-bad
+      gst_all_1.gst-plugins-ugly
+      gst_all_1.gst-libav
+      gst_all_1.gst-vaapi
+      clapper
+      gjs
+      gtk4
+      glib
+      wayland
+      wayland-protocols
     ];
+
     dontWrapGApps = true;
 
-#    installPhase = ''
-#      cp -r $out/share/gsettings-schemas/gnome-extension-hanabi-/glib-2.0 $out/share/glib-2.0
-#    '';
-    
     postPatch = ''
       patchShebangs build-aux/meson-postinstall.sh 
     '';
 
-#    postInstall = ''
-#      mv "$out/share/glib-2.0/schemas" "$out/share/gnome-shell/extensions/hanabi-extension@jeffshee.github.io/schemas"
-#    '';
-
     postFixup = ''
       wrapGApp "$out/share/gnome-shell/extensions/hanabi-extension@jeffshee.github.io/renderer/renderer.js"
-      ln -s "$out/share/gsettings-schemas/gnome-ext-hanabi-/glib-2.0/schemas" "$out/share/gnome-shell/extensions/hanabi-extension@jeffshee.github.io/schemas"
 
+      ln -s "$out/share/gsettings-schemas/${pname}-${version}/glib-2.0/schemas" \
+        "$out/share/gnome-shell/extensions/hanabi-extension@jeffshee.github.io/schemas"
     '';
 
+    # git ls-remote https://github.com/jeffshee/gnome-ext-hanabi for rev
     src = fetchFromGitHub {
       owner = "jeffshee";
       repo = "gnome-ext-hanabi";
-      rev = "10c0460b12bf0d6e42e50b9f0071201671059a70";
-      sha256 = "sha256-O7C5CadzMtw6VwSOtMJ6R+C4sjl5GG1WZDF45Vay2oo=";
+      rev = "b02101014a34ba053edaa64e2ec142d0d2f0f6f9";
+      sha256 = "sha256-IkQtg0fddCnEy2yGKtH5ZOdbuclJLkCqBjO2pSeUk9I=";
     };
   };
 }
-
-# git ls-remote https://github.com/jeffshee/gnome-ext-hanabi for rev
