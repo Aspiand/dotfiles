606740537d104d78e2918dc574fd0517afcc598b
Aspian <muhammad.aspian.d@gmail.com>
2026-04-26 18:57:03 +0800

    home-manager(ssh): add dalet

--- home-manager/configs/ssh.nix
+++ home-manager/configs/ssh.nix
@@ -6,6 +6,15 @@
     enableDefaultConfig = false;
 
     matchBlocks = {
+      dalet = {
+        hostname = "192.168.7.6";
+        user = "nix-on-droid";
+        port = 3022;
+        identityFile = [
+          "~/.ssh/id_ed25519"
+        ];
+      };
+
       self = {
         hostname = "agarta";
         port = 23231;
