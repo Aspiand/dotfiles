9e37adc0ef78f20deef7094f3985b332ca72e408
Aspian <muhammad.aspian.d@gmail.com>
2026-05-17 17:53:12 +0800

    update fresh

--- home-manager/configs/fresh.nix
+++ home-manager/configs/fresh.nix
@@ -1,4 +1,18 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   xdg.configFile."fresh/config.json".source = config.lib.file.mkOutOfStoreSymlink ./fresh.json;
+
+  home.packages = with pkgs; [
+    # Language Servers
+    bash-language-server
+    dockerfile-language-server
+    gopls
+    lua-language-server
+    marksman
+    nixd
+    phpactor
+    pyright
+    yaml-language-server
+    vscode-langservers-extracted # HTML, CSS, JSON, ESLint
+  ];
 }
