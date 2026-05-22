8bccf4048997de025c2e67c0d6751b90906adca1
Aspian <muhammad.aspian.d@gmail.com>
2026-05-22 15:40:17 +0800

    nix: add tlauncher

---
+++ nix/tlauncher.nix
@@ -0,0 +1,85 @@
+{ ... }:
+
+let
+  mkTLauncher =
+    pkgs:
+    pkgs.callPackage (
+      {
+        lib,
+        stdenv,
+        fetchurl,
+        unzip,
+        makeDesktopItem,
+        copyDesktopItems,
+        makeWrapper,
+        jre,
+      }:
+      stdenv.mkDerivation rec {
+        pname = "tlauncher";
+        version = "2.83";
+
+        src = fetchurl {
+          url = "https://tlauncher.org/jar";
+          hash = "sha256-xht2qWiXbOi6ezHziEcSVCxN6BPKDG8yQZSvULnLRW8=";
+        };
+
+        dontUnpack = true;
+
+        nativeBuildInputs = [
+          unzip
+          makeWrapper
+          copyDesktopItems
+        ];
+
+        desktopItems = [
+          (makeDesktopItem {
+            name = "tlauncher";
+            exec = "tlauncher";
+            icon = "tlauncher";
+            desktopName = "TLauncher";
+            comment = meta.description;
+            categories = [ "Game" ];
+          })
+        ];
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/share/tlauncher
+          unzip -j $src "TLauncher.jar" -d $out/share/tlauncher
+          mv $out/share/tlauncher/TLauncher.jar $out/share/tlauncher/tlauncher.jar
+
+          makeWrapper ${jre}/bin/java $out/bin/tlauncher \
+            --add-flags "-jar $out/share/tlauncher/tlauncher.jar"
+
+          runHook postInstall
+        '';
+
+        meta = with lib; {
+          description = "Custom Minecraft Launcher";
+          homepage = "https://tlauncher.org";
+          license = licenses.unfree;
+          platforms = platforms.linux;
+          mainProgram = "tlauncher";
+        };
+      }
+    ) { };
+in
+{
+  flake.lib.tlauncher.mkPackage = mkTLauncher;
+
+  flake.overlays.tlauncher = final: _: {
+    tlauncher = mkTLauncher final;
+  };
+
+  perSystem =
+    { pkgs, ... }:
+    let
+      tlauncher = mkTLauncher pkgs;
+    in
+    {
+      packages = {
+        inherit tlauncher;
+      };
+    };
+}
