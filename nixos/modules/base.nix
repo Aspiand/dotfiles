fd0e149bcbabc4f173d6bb7c9b556ec05e3c4fcc
Aspian <muhammad.aspian.d@gmail.com>
2026-07-10 19:08:07 +0800

    anu

--- nixos/modules/base.nix
+++ nixos/modules/base.nix
@@ -3,51 +3,58 @@
     { config, lib, ... }:
     let
       mkDefaults = (import ../../lib { inherit lib; }).mkDefaults;
+      inherit (lib) mkDefault;
     in
     {
-      config = mkDefaults {
-        time.timeZone = "Asia/Makassar";
-        nixpkgs.config.allowUnfree = true;
+      sops.secrets.env = {
+        sopsFile = ../../secrets/common.yml;
+      };
 
-        nix = {
-          gc = {
-            automatic = true;
-            dates = "weekly";
-            options = "--delete-older-than 7d";
-          };
+      environment.extraInit = ''
+        . /run/secrets/env
+      '';
 
-          settings = {
-            auto-optimise-store = true;
-            trusted-users = lib.mkForce [ "@wheel" ];
-            substituters = lib.mkForce [
-              "https://cache.nixos.org"
-              "https://nix.aspian.my.id"
-            ];
-            trusted-public-keys = lib.mkForce [
-              "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-              "github-ci-2:eUvIhhjHCO/kJVGcFNd/sNCGSx59tj1QAXmb477OO00="
-            ];
-            experimental-features = [
-              "nix-command"
-              "flakes"
-              "pipe-operators"
-            ];
-          };
+      time.timeZone = mkDefault "Asia/Makassar";
+      nixpkgs.config.allowUnfree = true;
+
+      nix = {
+        gc = {
+          automatic = true;
+          dates = "weekly";
+          options = "--delete-older-than 7d";
         };
 
-        i18n = {
-          defaultLocale = "en_US.UTF-8";
-          extraLocaleSettings = {
-            LC_ADDRESS = "id_ID.UTF-8";
-            LC_IDENTIFICATION = "id_ID.UTF-8";
-            LC_MEASUREMENT = "id_ID.UTF-8";
-            LC_MONETARY = "id_ID.UTF-8";
-            LC_NAME = "id_ID.UTF-8";
-            LC_NUMERIC = "id_ID.UTF-8";
-            LC_PAPER = "id_ID.UTF-8";
-            LC_TELEPHONE = "id_ID.UTF-8";
-            LC_TIME = "id_ID.UTF-8";
-          };
+        settings = {
+          auto-optimise-store = true;
+          trusted-users = [ "@wheel" ];
+          substituters = [
+            "https://cache.nixos.org"
+            "https://nix.aspian.my.id"
+          ];
+          trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "github-ci-2:eUvIhhjHCO/kJVGcFNd/sNCGSx59tj1QAXmb477OO00="
+          ];
+          experimental-features = [
+            "nix-command"
+            "flakes"
+            "pipe-operators"
+          ];
+        };
+      };
+
+      i18n = mkDefaults {
+        defaultLocale = "en_US.UTF-8";
+        extraLocaleSettings = {
+          LC_ADDRESS = "id_ID.UTF-8";
+          LC_IDENTIFICATION = "id_ID.UTF-8";
+          LC_MEASUREMENT = "id_ID.UTF-8";
+          LC_MONETARY = "id_ID.UTF-8";
+          LC_NAME = "id_ID.UTF-8";
+          LC_NUMERIC = "id_ID.UTF-8";
+          LC_PAPER = "id_ID.UTF-8";
+          LC_TELEPHONE = "id_ID.UTF-8";
+          LC_TIME = "id_ID.UTF-8";
         };
       };
     };
